07a2f01acd9f71fc64f4900d6d75ed107a0abcd7
dragonbitestail <dbt@example.com>
2025-12-08 10:33:06 -0500

    Adjust modtime of each montage out file
    To keep montage file next to it's input file in file manager when ordered by modtime
    we are now setting modtime of each montage file to same as input file.
    
    Additional:
      * Refactored debug messages to more concise form.
      * Trimmed trailing whitespace
    
      * Fixed file type check. Now only allows media types as listed in grep
        conditional. On fail, loop continues to next file instead of exiting.
    
      * Swapped all zentity commands to yad. This fixed inability to set the
        initial directory in the file chooser for the case where arg 1 to the
        script was a directory.

--- moviethumbs.sh
+++ moviethumbs.sh
@@ -6,9 +6,9 @@
 # zentity, imagemagick, ffmpegthumbnailer, trash-cli
 #
 # Initial test environment:
-# * Ubuntu 12.04.5 LTS 
+# * Ubuntu 12.04.5 LTS
 # * XFCE 4.8  (Window Manager)
-# * Thunar 
+# * Thunar
 # * Deployed as a Thunar file manager "custom action"
 #
 
@@ -51,8 +51,11 @@ ffmpegthumbnailer=/usr/bin/ffmpegthumbnailer
 montage=/usr/bin/montage
 tr=/usr/bin/tr
 ls=/bin/ls
+stat=/usr/bin/stat
+cut=/usr/bin/cut
+touch=/usr/bin/touch
 trash=/usr/bin/trash-put
-zenity=/usr/bin/zenity
+yad=/usr/bin/yad
 
 SELECT_FILE_DIR=""
 FILEPATHS=( )
@@ -65,35 +68,30 @@ if [[ $# -gt 0 ]]; then
   if [ -d "$1" ];then # First arg is directory. Assume we will be selecting files using dialog.
     SELECT_FILE_DIR="$1"
     shift
-    if $DEBUG; then 
-      $echo "arg 1 is directory = \"$SELECT_FILE_DIR\""
-    fi
+    $DEBUG && $echo "arg 1 is directory = \"$SELECT_FILE_DIR\""
   fi
   FILEPATHS=( "$@" )
 fi
-
 
 ZENITY_FILEPATHS=( )
 if [ -d "$SELECT_FILE_DIR" ];then
   IFS_pre=$IFS
   IFS="|"
-  ZENITY_FILEPATHS=( $( $zenity --file-selection --filename="$SELECT_FILE_DIR" --multiple ) )
+  ZENITY_FILEPATHS=( $( $yad --file-selection --filename="$SELECT_FILE_DIR" --multiple --heightr=300 --width=1200) )
   $echo "ZENITY_FILEPATHS = ${ZENITY_FILEPATHS[@]}"
   IFS=$IFS_pre
   FILEPATHS=( "${FILEPATHS[@]}" "${ZENITY_FILEPATHS[@]}" )
 
 fi
 
-if $DEBUG; then
-  $echo "FILEPATHS following optional zenity select: ${FILEPATHS[@]}"
-fi
+$DEBUG && $echo "FILEPATHS following optional yad select: ${FILEPATHS[@]}"
 
 if [[ ${#FILEPATHS[@]} -lt 1 ]]; then  # Not enough args, bail out.
-  $zenity  --info --text="No file/s selected. Exiting..."
+  $yad  --info --text="No file/s selected. Exiting..."
   exit 1
 fi
 if [[ ${#FILEPATHS[@]} -gt 1 ]]; then  # Multiple files, Query whether to apply settings to all files
-  if zenity --question --title="Sequence settings" --text="Apply same settings to all?"; then
+  if yad --question --title="Sequence settings" --text="Apply same settings to all?"; then
     SETTINGS_GLOBAL=true
   fi
 fi
@@ -101,8 +99,8 @@ fi
 # For each file in $FILEPATHS do
 #   * Test the file for compliance w/ our rules
 #   * cd to the directory where our selected files reside.
-#   * Create a temp dir, as child-dir of cwd, based on the current file selected in our loop 
-#   * Query for snapshot sequence vals. 
+#   * Create a temp dir, as child-dir of cwd, based on the current file selected in our loop
+#   * Query for snapshot sequence vals.
 #   * Create set of snapshot stills from current selected movie file and write to temp dir.
 #   * Create Montage image from the stills based on name of the movie file
 #   * Prompt to perform cleanup of the temp dir.
@@ -110,43 +108,52 @@ for FILEPATH in "${FILEPATHS[@]}"; do
 
   # Testing file
   FILEINFO=$( $file "$FILEPATH" )
-  if $echo $FILEINFO | $grep -qviE 'video|asf|mkv|mpeg|mpg|RealMedia|flv|avi|mp4' > /dev/null 2>&1 ; then
-    $zenity  --info --text="The \"file\" command does not recognize $FILEPATH as a video file.\n\nFile Info: $FILEINFO\n\nExiting..."
-    exit 1
+  if $echo $FILEINFO | $cut -d':' -f2 | \
+     $grep -qiE 'video|asf|mkv|mpeg|mpg|RealMedia|flv|avi|mp4' \
+     > /dev/null 2>&1 ; then
+    $DEBUG && $echo "Passed file check with info: $FILEINFO"
+  else
+    $yad  --info --text="The \"file\" command does not recognize $FILEPATH \
+      as a video file.\n\nFile Info: $FILEINFO"
+    #exit 1
+    continue
   fi
-  
+
+  infile_mtime="$($stat "$FILEPATH"  | $grep Modify:  | $cut -d ':' -f 2- | $cut -d ' ' -f2-)"
+  $DEBUG && $echo "[DEBUG] \"$FILEPATH\" mtime: $infile_mtime"
+
   FILENAME=$( $bname "$FILEPATH" )
-  
+
   START_DIR=$( $dname "$FILEPATH" )
   cd "$START_DIR"
-  
-  SNAP_DIR=$( $echo -n montage_$FILENAME | $tr -c "[:alnum:]" "." )
-  
-  $mkdir -p $SNAP_DIR
-  
-  cd $SNAP_DIR
-  
-    # Unfortunately forms does not seem to support setting default values
-    # which results in multiple prompts. 
-  if $SEQ_GETVALS; then
-    SEQ_START=$( $zenity --entry --text="Start % (0-100)" --entry-text=$SEQ_START )
-    SEQ_STEP=$( $zenity --entry --text="Step from start to 100" --entry-text=$SEQ_STEP )
 
-    SEQ_THUMB_SIZE=$( $zenity --entry --text="Pixel width size of thumbs" --entry-text=$SEQ_THUMB_SIZE )
+  SNAP_DIR=$( $echo -n montage_$FILENAME | $tr -c "[:alnum:]" "." )
+
+  $mkdir -p $SNAP_DIR
+
+  cd $SNAP_DIR
+
+    # Unfortunately forms does not seem to support setting default values
+    # which results in multiple prompts.
+  if $SEQ_GETVALS; then
+    SEQ_START=$( $yad --entry --text="Start % (0-100)" --entry-text=$SEQ_START )
+    SEQ_STEP=$( $yad --entry --text="Step from start to 100" --entry-text=$SEQ_STEP )
+
+    SEQ_THUMB_SIZE=$( $yad --entry --text="Pixel width size of thumbs" --entry-text=$SEQ_THUMB_SIZE )
 
   fi
 
   if $SETTINGS_GLOBAL; then  # When true, allow it to only happen on first loop iteration.
     SEQ_GETVALS=false
   fi
-  
+
   $echo "In \"$( pwd  )\" w/ Sequence start / step: $SEQ_START /  $SEQ_STEP" >> $LOG_FILE 2>&1
-  
+
   # Ensure $? is 0 prior to snapshots loop
   true
   for i in $( seq $SEQ_START $SEQ_STEP 100  ); do
     pnum=$( $printf "%03d" $i )
-   
+ 
     if [ ! -f "${SNAP_DIR}_${pnum}.jpg" ]; then
       thumbs_cmd="$ffmpegthumbnailer -f -q10 -i ../\"$FILENAME\"  -o ${SNAP_DIR}_${pnum}.jpg -t\"${i}%\" -s\"${SEQ_THUMB_SIZE}\"   >> $LOG_FILE 2>&1"
       $echo  "$thumbs_cmd"   >> $LOG_FILE 2>&1
@@ -155,39 +162,41 @@ for FILEPATH in "${FILEPATHS[@]}"; do
     else
       $echo  "\"${SNAP_DIR}_${pnum}.jpg\" already exists. Skipping..."   >> $LOG_FILE 2>&1
     fi
-    $echo $i  # Echo the snapshot percentage just completed for zenity
-  
+    $echo $i  # Echo the snapshot percentage just completed for yad
+
     EXIT_VAL=$?
-  
+
     $echo  "Current exit value = $EXIT_VAL" >> $LOG_FILE 2>&1
     if [ "$EXIT_VAL" != "0" ];then
       $echo  "exiting. Assume Canceled."   >> $LOG_FILE 2>&1
       break
-    fi 
-  
-  done | $zenity --progress --auto-close
-  
+    fi
+
+  done | $yad --progress --auto-close
+
   # SNAPS_ERROR val eaten by pipe while $? has a non-0 exit value (although not the val from within the pipe).
   # $? being used for now.  If run into issues explore PIPESTATUS.
-  # $echo  "SNAPS_ERROR = \"$SNAPS_ERROR\" following for loop|zenity..."   >> $LOG_FILE 2>&1
-  # $echo  "$? = \"$?\" following for loop|zenity..."   >> $LOG_FILE 2>&1
+  # $echo  "SNAPS_ERROR = \"$SNAPS_ERROR\" following for loop|yad..."   >> $LOG_FILE 2>&1
+  # $echo  "$? = \"$?\" following for loop|yad..."   >> $LOG_FILE 2>&1
   if [ "$?" != "0" ];then
     SNAPS_ERROR="Snapshots Canceled/Errored..."
-  fi 
-  
+  fi
+
   BASEFILE=${FILENAME%.*}
-  
+
   # Create a montage sheet from all the snapshots:
-  
+
   $montage -background Black -pointsize 28 -stroke White -fill Yellow -gravity south -title "${FILENAME%.*}" -geometry '1x1+8+8<'  ${SNAP_DIR}_*.jpg   ../"$BASEFILE".jpg  >> $LOG_FILE 2>&1
   $sleep 0.4
-  
+
+  $touch -d "$infile_mtime" -m ../"$BASEFILE".jpg
+
   # Prompt to Cleanup our SNAP_DIR
   # BUG on NAS drive:
   # "/bin/ls: cannot access *.jpg: No such file or directory"
-    
+
   if $DO_PURGE_QUERY; then
-    if $zenity  --question --text="${SNAPS_ERROR}\nPurge the Snapshots directory \"$SNAP_DIR\"?" ; then
+    if $yad  --question --text="${SNAPS_ERROR}\nPurge the Snapshots directory \"$SNAP_DIR\"?" ; then
       DO_PURGE=true
     else
       DO_PURGE=false
